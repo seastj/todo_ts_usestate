adc01fb8688363753c623044762ebabd5c7c077a
seastj <dev.seastj@gmail.com>
2025-08-23 23:56:44 +0900

    [docs] 글등록 작업중

--- src/todos/TodoWrite.tsx
+++ src/todos/TodoWrite.tsx
@@ -1,5 +1,21 @@
+import { ChangeEvent, useState } from 'react';
+import { ITodoType } from '../types/todoTypes';
+
+interface TodoWriteProps {}
+
 function TodoWrite() {
-  return <div>TodoWrite</div>;
+  const [text, setText] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={e => handleChange(e)} />
+      <button>등록하기</button>
+    </div>
+  );
 }
 
 export default TodoWrite;
