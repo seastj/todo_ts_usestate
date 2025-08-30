e6f4daa186aaad22e5e0877f8ef49f73226fcfd5
seastj <dev.seastj@gmail.com>
2025-08-30 23:42:06 +0900

    [docs] 헤더 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 function Header() {
   return (
-    <div className="flex justify-center p-10 bg-gray-50 border border-b-gray-200 font-semibold text-3xl">
+    <div className="flex justify-between p-10 bg-gray-50 border border-b-gray-200 font-semibold text-3xl">
       <span>My Todo</span>
-      <span>다크모드</span>
-      <span>내정보</span>
+      <div className="flex gap-10 text-xl font-normal">
+        <span>다크모드</span>
+        <span>내정보</span>
+      </div>
     </div>
   );
 }
